1745f9b8085352060108f5afa739119fc8e99951
VuVanDuc <vuduc19081997@gmail.com>
2020-03-09 16:34:26 +0700

    Django Tutorial Part 4: Django admin site

--- mysite/catalog/admin.py
+++ mysite/catalog/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book, Author, Genre, BookInstance
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+admin.site.register(Book)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+admin.site.register(BookInstance)
