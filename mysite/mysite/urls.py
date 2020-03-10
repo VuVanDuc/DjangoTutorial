543847ff10a243df3f3d1861de859511e17dee13
VuVanDuc <vuduc19081997@gmail.com>
2020-03-10 12:06:54 +0700

    Django Tutorial Part 5: Creating our home page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,7 +21,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('catalog/', include('catalog.urls')),
+    path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
 
