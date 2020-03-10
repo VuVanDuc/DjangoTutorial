d26d09295092e48d4bf17db22653ded812f5ef76
VuVanDuc <vuduc19081997@gmail.com>
2020-03-10 12:12:53 +0700

    Xoa thu muc .idea
    Update gitignore
    
    fix endline
    
    update mysite/gitignore
    
    reformat file catalog models
    
    Part 3: Models
    
    Xoa thu muc .idea
    
    fix endline
    
    update mysite/gitignore
    
    reformat file catalog models
    
    xoa thu muc migrations, update vao file gitignore

--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -2,7 +2,6 @@ import uuid
 
 from django.db import models
 
-
 # Create your models here.
 from django.urls import reverse
 
@@ -43,7 +42,8 @@ class Book(models.Model):
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text='Unique ID for this particular book across whole library')
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
